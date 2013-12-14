945385a7cccb18d864aabd37d3f168c9d58d9c96
sunya123 <sunyanhao@jd.com>
2013-12-14 17:30:27 +0800

    add  some test

--- src/sqlite/sql.py
+++ src/sqlite/sql.py
@@ -2,9 +2,10 @@ import logging
 import sqlite3
 
 #logging.basicConfig(filename='debug.log',level=logging.DEBUG)
-logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',level=logging.DEBUG)
+#logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',level=logging.DEBUG)
 
 class SQLAgent(object):
+
 	"""SqlAgent is a common interface to operate  sqlite3 database"""
 	def __init__(self,name):
 		super(SQLAgent, self).__init__()
@@ -20,12 +21,12 @@ class SQLAgent(object):
 	"""
 	def createDb(self):
 		self._conn=sqlite3.connect(self.name)
-		logging.debug("create database")
+		logging.debug("start create database")
 		if self._conn:
 			return  self._conn
 		else:
 			logging.error("create database failed")
-			return None
+			raise Exception("create database error!")
 
 	"""
 	@return  get the cursor of the database
@@ -44,14 +45,27 @@ class SQLAgent(object):
 	@type  list
 
 	"""
-	def _execute(self,sql,params):
-		self._cursor.execute(sql,params)
+	def _execute(self,sql,params=None):
+		print "------start execute-----"
+		if self._cursor:
+			logging.debug(sql)
+			if params:
+				self._cursor.execute(sql,params)
+			else:
+				logging.debug("params is none")
+				logging.debug(sql)
+				self._cursor.execute(sql)
+		else:
+			logging.debug("fail to execute sql:%s"%(sql))
+			raise  ValueException("NoneType")
 
 	def _executemany(self,sql,params):
 		self._cursor.executemany(sql,params)
 
 	def commit(self):
-		self._cursor.commit()
+		if self._cursor:
+			logging.debug("start to commit the operate")
+			self._conn.commit()
 
 	def fetchone(self):
 		return self._cursor.fetchone()
@@ -62,10 +76,39 @@ class SQLAgent(object):
 	def fetchall(self):
 		return  self._cursor.fetchall()
 
-	def  query(self,sqlstring,param):
-		self._execute(sqlstring,param)
-
-	def execute(self,sql,param):
+	def query(self,sql,param=None):
 		self._execute(sql,param)
-		self.commit()
+
+	def executeTable(self,sql,param=None):
+		logging.debug("begin create  table")
+		if param:
+			self._execute(sql,param)
+		else:
+			self._execute(sql)
+
+
+	def execute(self,sql,param=None):
+		try:
+			if param:
+				logging.debug("start execute sql:%s:%s"%(sql,param))
+				self._execute(sql,param)
+			else:
+				logging.debug("start execute sql:%s"%(sql))
+				self._execute(sql)
+			self.commit()
+		except:
+			raise  Exception('execute sql:%s failed'%(sql))
+# def main():
+# 	sa=SQLAgent("./mydb.db")
+# 	sa.createDb()
+# 	sa.getCursor()
+# 	sql='''create table test(date text, trans text, symbol text, qty real, price real)'''
+# 	sa.executeTable(sql)
+# 	sql="""insert into test
+#           values ('2006-01-05','BUY','RHAT',100,35.14)"""
+# 	sa.execute(sql)
+
+# if __name__ == '__main__':
+# 		main()
+
 
--- src/test/sql_test.py
+++ src/test/sql_test.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from os import path
 if not os.getenv("PYTHONPATH"):
 	parent=os.path.split(os.path.realpath(__file__))[0] 
@@ -6,28 +7,60 @@ if not os.getenv("PYTHONPATH"):
 import unittest
 from sqlite import sql
 
+logging.basicConfig(format='%(asctime)s %(funcName)s %(levelname)s:%(message)s',level=logging.DEBUG)
+
 class  TestSQLAgent(unittest.TestCase):
 
 	def setUp(self):
-		self.db=sql.SQLAgent("./mydb.db")
-		self.db.createDb()
-		
+		self.agent=sql.SQLAgent("./mydb.db")
+
 	def tearDown(self):
-		self.db=None
+		self.agent=None
 		os.remove("./mydb.db")
 
 	def test_creatDb(self):
-		if self.db:
-			self.assertNotEqual(self.db.name,None)
-			self.assertNotEqual(self.db.createDb(),None)
+		if self.agent:
+			self.agent.createDb()
+			self.assertNotEqual(self.agent.name,None)
+			self.assertNotEqual(self.agent.createDb(),None)
 		else:
 			self.assertTrue(False)
 
 	def test_getCursor(self):
-		if self.db:
-			self.assertNotEqual(self.db.getCursor(),None)
+		if self.agent:
+			self.agent.createDb()
+			self.assertNotEqual(self.agent.getCursor(),None)
 		else:
 			self.assertTrue(False)
+
+	def test_createTable(self):
+		if self.agent:
+			sql='''create table test
+                 (date text, trans text, symbol text,
+                    qty real, price real)'''
+			self.agent.createDb()
+			self.agent.getCursor()
+			self.agent.executeTable(sql)
+			sql="drop table test"
+			self.agent.executeTable(sql)
+
+	def test_execute(self):
+		if self.agent:
+			self.agent.createDb()
+			self.agent.getCursor()
+			sql='''create table test
+                 (date text, trans text, symbol text,
+                    qty real, price real)'''
+
+			self.agent.executeTable(sql)
+			sql="""insert into test values ('2006-01-05','BUY','RHAT',100,35.14)"""
+			self.agent.execute(sql)
+			sql='insert into test values(?,?,?,?,?)'
+			param=('2006-01-07','SALE','RH',10,30.14)
+			self.agent.execute(sql,param)
+		else:
+			self.assertTrue(False)
+
 
 		
 
